b6cf723dbe38111a96c6dc69db87ac067a133609
Peter Prescott <prescott.peter@gmail.com>
2021-10-11 10:02:02 +0100

    Generalize test for improved solution (#6)

--- tests/test_improved.py
+++ tests/test_improved.py
@@ -2,24 +2,74 @@
 PyTest tests for attempted improvements.
 """
 
-from opt_nn import given, improved
+from opt_nn import given, improved, kdtree
 
 
-def test_less_slow():
+def check_solution(alternative_solution):
+    '''
+    Check that improved solution returns same values as given slow solution.
+    '''
 
     df = given.make_data(100)
 
     # need to copy df to compare answers as `slow()` modifies inplace
     a0 = given.slow(df.copy())
-    a1 = improved.less_slow(df.copy())
+    a1 = alternative_solution(df.copy())
 
-    compare_distance = a1.distance_km == a0.distance_km
-    compare_index = a1.neighbour_index == a0.neighbour_index
+    # compare equality of distances
+    compare_distance = (a1.distance_km == a0.distance_km)
+    # compare equality of neighbour indices
+    compare_index = (a1.neighbour_index == a0.neighbour_index)
 
     assert len(compare_distance.unique()) == 1
     assert compare_distance.unique()[0] == True
     assert len(compare_index.unique()) == 1
     assert compare_index.unique()[0] == True
+
+
+def wrong_solution(points_df):
+    '''
+    Return same answers as `slow()` solution,
+    but change zeroth neighbour_index to impossible negative value.
+    '''
+
+    df = given.slow(points_df)
+    df.loc[0, 'neighbour_index'] = -1
+
+    return df
+
+
+def test_check_solution():
+    '''
+    Test that check_solution passes for identical `given.slow` solution 
+    and fails for `wrong_solution`.
+    '''
+
+    # check_solution passes for identical solution
+    check_solution(given.slow)
+
+    # check_solution fails for wrong_solution
+    try:
+        check_solution(wrong_solution)
+    except Exception as e:
+        # `wrong_solution` should throw an AssertionError
+        assert isinstance(e, AssertionError)
+
+
+def test_less_slow():
+    '''
+    Test `improved.less_slow()` solution.
+    '''
+
+    check_solution(improved.less_slow)
+
+
+def test_use_kdtree():
+    '''
+    Test `kdtree.use_kdtree()` solution.
+    '''
+
+    check_solution(kdtree.use_kdtree)
 
 
 def test_h_distance():
