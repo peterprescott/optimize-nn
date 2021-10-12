24d71718b485452a0d8c7f11d41dd088e548b040
Peter Prescott <prescott.peter@gmail.com>
2021-10-12 09:05:59 +0100

    Revert module name for 2-d spherical kd-tree

--- src/opt_nn/profile.py
+++ src/opt_nn/profile.py
@@ -65,7 +65,7 @@ if __name__=='__main__':
 
     from opt_nn.given import slow
     from opt_nn.improved import less_slow
-    from opt_nn.spherical_kdtree import use_kdtree
+    from opt_nn.kdtree import use_kdtree
     from opt_nn.xyz import use_3dtree
 
     solutions = [slow, less_slow, use_kdtree, use_3dtree]
